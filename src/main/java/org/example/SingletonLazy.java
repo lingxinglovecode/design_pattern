615923cac609f9d8527b9e0729d814e7dbd2eef1
wanglianxing <lianxing_wang@smail.nju.edu.cn>
2024-03-01 13:28:00 +0800

    单例模式: 创建一个全局使用的对象 非线程安全懒汉式: 只有在需要单例对象的时候才会对其进行创建,此实现非线程安全,当多线程情况下存在创建多次单例对象的情况

---
+++ src/main/java/org/example/SingletonLazy.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class SingletonLazy {
+
+    // 存储单例对象的属性必须是静态且私有的,静态是因为单例类的构造器是私有的,对外只提供一个获取单例对象
+    // 的方法,由于外部不能直接创建对象,所以获取单例对象的方法一定是静态的,而在其中的单例类也一定为静态
+    // 私有是防止该对象直接被外部获取,必须通过方法获取对象保证其始终不会被改变
+    private static SingletonLazy instance;
+    private SingletonLazy(){}
+
+    public static SingletonLazy getInstance(){
+        if(instance == null){
+            instance = new SingletonLazy();
+        }
+        return instance;
+    }
+}
